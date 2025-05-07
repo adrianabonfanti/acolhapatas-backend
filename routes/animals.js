b4a7c3641792f0e34bdea5aec0a9904f45f97d23
wdev-adrianabonfanti <adriana.bonfanti@ebix.com>
2025-05-07 13:02:42 -0300

    Deploy ajustes animals js5 novamente

--- routes/animals.js
+++ routes/animals.js
@@ -2,7 +2,7 @@ import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import upload from "../middlewares/upload.js";
 import Animal from "../models/Animal.js";
-
+import multer from "multer"; // se ainda não tiver essa linha no topo do arquivo
 const router = express.Router();
 
 // Buscar animais com filtros
@@ -51,35 +51,40 @@ router.post("/", authMiddleware, upload.array("fotos"), async (req, res) => {
     res.status(500).json({ message: "Erro ao cadastrar animal", error: err.message });
   }
 });
-
-// Atualizar animal
-router.put("/:id", authMiddleware, upload.array("fotos"), async (req, res) => {
-  try {
-    console.log("req.body:", req.body);
-    console.log("req.files:", req.files);
-    const atualizacao = {
-      ...req.body,
-      castrado: req.body.castrado === "true" || req.body.castrado === true,
-      vacinado: req.body.vacinado === "true" || req.body.vacinado === true,
-      precisaLarTemporario: req.body.precisaLarTemporario === "true" || req.body.precisaLarTemporario === true,
-      necessidadesEspeciais: req.body.necessidadesEspeciais === "true" || req.body.necessidadesEspeciais === true,
-      usaMedicacao: req.body.usaMedicacao === "true" || req.body.usaMedicacao === true,
-      deficiencia: req.body.deficiencia === "true" || req.body.deficiencia === true
-    };
-
-    if (req.files && req.files.length > 0) {
-      atualizacao.fotos = req.files.map((file) => file.path);
-    }
-
-    const animal = await Animal.findByIdAndUpdate(req.params.id, atualizacao, { new: true });
-    if (!animal) return res.status(404).json({ message: "Animal não encontrado" });
-    res.json(animal);
-  } catch (err) {
-    res.status(500).json({ message: "Erro ao atualizar animal", error: err.message });
-  }
-
-
+router.put("/:id", multer().any(), async (req, res) => {
+  console.log("TESTANDO REQUISIÇÃO...");
+  console.log("BODY:", req.body);
+  console.log("FILES:", req.files);
+  res.send("ok");
 });
+// Atualizar animal
+// router.put("/:id", authMiddleware, upload.array("fotos"), async (req, res) => {
+//   try {
+//     console.log("req.body:", req.body);
+//     console.log("req.files:", req.files);
+//     const atualizacao = {
+//       ...req.body,
+//       castrado: req.body.castrado === "true" || req.body.castrado === true,
+//       vacinado: req.body.vacinado === "true" || req.body.vacinado === true,
+//       precisaLarTemporario: req.body.precisaLarTemporario === "true" || req.body.precisaLarTemporario === true,
+//       necessidadesEspeciais: req.body.necessidadesEspeciais === "true" || req.body.necessidadesEspeciais === true,
+//       usaMedicacao: req.body.usaMedicacao === "true" || req.body.usaMedicacao === true,
+//       deficiencia: req.body.deficiencia === "true" || req.body.deficiencia === true
+//     };
+
+//     if (req.files && req.files.length > 0) {
+//       atualizacao.fotos = req.files.map((file) => file.path);
+//     }
+
+//     const animal = await Animal.findByIdAndUpdate(req.params.id, atualizacao, { new: true });
+//     if (!animal) return res.status(404).json({ message: "Animal não encontrado" });
+//     res.json(animal);
+//   } catch (err) {
+//     res.status(500).json({ message: "Erro ao atualizar animal", error: err.message });
+//   }
+
+
+// });
 
 // Excluir animal
 router.delete("/:id", authMiddleware, async (req, res) => {
