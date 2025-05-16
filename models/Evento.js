fc2dc3c32058d67f1c8717cc5e0919343a4fef16
wdev-adrianabonfanti <adriana.bonfanti@ebix.com>
2025-05-16 01:03:37 -0300

    Deploy cidade

--- models/Evento.js
+++ models/Evento.js
@@ -9,6 +9,8 @@ const eventoSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   local: { type: String, required: true },
   endereco: { type: String },
+  cidade: { type: String },
+estado: { type: String },
   data: { type: String, required: true }, // formato: "2025-05-20"
   horaInicio: { type: String, required: true }, // formato: "14:00"
   horaFim: { type: String, required: true }, // formato: "17:00"
