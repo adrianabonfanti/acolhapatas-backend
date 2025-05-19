4499b21470d1d20ad2da55e1a3411a855da00082
wdev-adrianabonfanti <adriana.bonfanti@ebix.com>
2025-05-19 16:54:57 -0300

    Deploy common js back routes4

--- routes/animalRoutes.js
+++ routes/animalRoutes.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // <- só essa linha deve ficar
 
 const authMiddleware = require('../middlewares/authMiddleware');
 const upload = require('../middlewares/upload');
-const { cadastrarAnimal } = require('../controllers/animalController'); // <- ESSENCIA
-
-const router = express.Router();
+const { cadastrarAnimal } = require('../controllers/animalController'); // <- ESSENCIAL
 
 // Rota para cadastro de animal (ONG logada)
 router.post('/ongs/animais', authMiddleware, upload.single("fotos"), cadastrarAnimal);
 
-
 module.exports = router;
-
